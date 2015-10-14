b97588e6b7be13d813a8f297acbd7cee1f1d4794
Wes Breisch <wabreisch@quinnipiac.edu>
2015-10-13 21:06:11 -0400

    Adds addition functionality

--- Arithmetic.java
+++ Arithmetic.java
@@ -1,5 +1,9 @@
 public class Arithmetic {
     public static void main(String[] args) {
-	System.out.println("Hello world");
+        //System.out.println(add(10,20));
+    }
+
+    public static int add(int a, int b) {
+        return a + b;
     }
 }
